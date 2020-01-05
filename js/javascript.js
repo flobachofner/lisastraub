8a622d2d84484dcfc62790e274ac320bb70db0c4
florianbachofner <florianbachofner@hotmail.com>
2020-01-05 16:55:05 +0100

    initial

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+$(window).scroll(function() {
+	var scrollTop = window.pageYOffset;
+	$(".backgroundfront").offset({top:scrollTop * 0.72 ,left:0})
+});
+
+$(window).scroll(function() {
+	var scrollTop = window.pageYOffset;
+	$(".siteTop").offset({top:scrollTop * 0.72 ,left:0})
+});
+
+$('iframe').load( function() {
+    $('iframe').contents().find("head")
+      .append($("<style type='text/css'>  .my-class{background-color:red;}  </style>"));
+});
