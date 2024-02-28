a085a3765b28f87d328676711535d7489db550f2
flo.bachofner <florianbachofner@hotmail.com>
2024-02-28 10:41:57 +0100

    fontawesome

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+$(window).scroll(function() {
+	var scrollTop = window.pageYOffset;
+	$(".backgroundfront").offset({top:scrollTop * 0.72 ,left:0})
+});
+
+$(window).scroll(function() {
+	var scrollTop = window.pageYOffset;
+	$(".siteTop").offset({top:scrollTop * 0.72 ,left:0})
+});
+
+$('iframe').load( function() {
+    $('iframe').contents().find("head")
+      .append($("<style type='text/css'>  .my-class{background-color:red;}  </style>"));
+});
+
+$(document).ready(function(){
+     $('#nav-mhweb-hamburger').click(function(){
+     $(this).toggleClass('open');
+   });
+});
+
+// Variablen Seminardaten
+function seminarDaten()
+{
+    return daten = {
+
+		heilklangkreis: "23. März 2024",
+	}
+}
+
